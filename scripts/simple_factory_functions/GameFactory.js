301481e7375d4d530f3570bf31c511e0f3cbdd35
Mathias Capdet <mathias.capdet@tescan.com>
2020-10-13 12:14:34 +0200

    Clean up factory function

--- scripts/simple_factory_functions/GameFactory.js
+++ scripts/simple_factory_functions/GameFactory.js
@@ -2,27 +2,32 @@ var GameFactory = (()=>{
     'use strict';
 
     const FixedStepFrame = ({update, render}) => o => {
-        var _gdt = 0;
-        var _step = 1/60;
-        var _last = null;
-        var _now = null;
-        var _dt = 0;
+        let _gdt = 0;
+        let _step = 1/60;
+        let _last = null;
+        let _now = null;
+        let _dt = 0;
+        let _frame = function(t) {
+            _now = t;
+            _last = _last || (t - _step * 1000);
+            _dt = Math.min((_now - _last) / 1000, 1);
+            _gdt += _dt;
+            while (_gdt >= _dt) {
+                this.update();
+                _gdt -= _dt;
+            }
+            this.render();
+            _last = _now;
+            requestAnimationFrame(_frame);
+        };
         return {
             ...o,
             update,
             render,
-            frame: function(t) {
-                _now = t;
-                _last = _last || (t - _step * 1000);
-                _dt = Math.min((_now - _last) / 1000, 1);
-                _gdt += _dt;
-                while (_gdt >= _dt) {
-                    this.update();
-                    _gdt -= _dt;
-                }
-                this.render();
-                _last = _now;
-            }
+            run: function() {
+                _frame = _frame.bind(this);
+                requestAnimationFrame(_frame);
+            },
         };
     };
 
